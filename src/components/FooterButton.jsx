c2c0092b02ee3de9061e2992b099045ef99ede7c
Shamil Kh <shamilist90@gmail.com>
2023-08-23 22:01:35 +0300

    add components

---
+++ src/components/FooterButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const FooterButton = () => {
+    return (
+        <div className='footerButton'>
+            <span className='footerButton__text'>Показать еще 5 билетов!</span>
+        </div>
+    );
+}
