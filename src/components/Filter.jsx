75c654adf0772a468439c008b72e61af300e3037
Shamil Kh <shamilist90@gmail.com>
2023-08-26 10:38:30 +0300

    filter

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 export const Filter = () => {
     return (
         <div className='filter'>
+            <p className="filter_text">КОЛИЧЕСТВО ПЕРЕСАДОК</p>
         </div>
     );
 }
