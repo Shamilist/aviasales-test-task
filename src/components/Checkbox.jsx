cc630baf6c4fb310a67f74a1bd5da88c019186cd
Shamil Kh <shamilist90@gmail.com>
2023-08-28 18:04:55 +0300

    filter style

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
 
 export const Checkbox = ({name, id}) => {
+    const [checked, isChecked] = useState(false);
+
+    const handleClick = () => {
+        isChecked(checked => !checked);
+        console.log('checked', checked)
+    }
 
     return (
         <div className='checkbox' >
-        <input  className="checkbox_input" type="checkbox" id={id}/>
-        <label className='checkbox_label' htmlFor={id} >{name}</label>
+        <input  className="checkbox_input" type="checkbox" id={id} onClick={handleClick}/>
+        <label className='checkbox_label' htmlFor={id}>{name}</label>
         </div>
     );
 }
