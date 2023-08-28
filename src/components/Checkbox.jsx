dbccc34dcf2fe4b62a279b4266abb936e50c1063
Shamil Kh <shamilist90@gmail.com>
2023-08-28 23:30:23 +0300

    fix

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -9,9 +9,9 @@ export const Checkbox = ({name, id}) => {
     }
 
     return (
-        <div className='checkbox' >
-        <input  className="checkbox_input" type="checkbox" id={id} onClick={handleClick}/>
-        <label className='checkbox_label' htmlFor={id}>{name}</label>
+        <div className='checkbox'>
+        <input  className="checkbox_input" type="checkbox" id={id}/>
+        <label htmlFor={id} onClick={handleClick} >{name}</label>
         </div>
     );
 }
