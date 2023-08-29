d11de28c2d46cc209bd30709b1a6234279e62121
Shamil Kh <shamilist90@gmail.com>
2023-08-29 17:13:11 +0300

    fix filter

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -5,11 +5,11 @@ export const Filter = () => {
     return (
         <div className='filter'>
             <p className="filter_text">КОЛИЧЕСТВО ПЕРЕСАДОК</p>
-            <Checkbox name="Все"/>
-            <Checkbox name="Без пересадок"/>
-            <Checkbox name="1 переcадка"/>
-            <Checkbox name="2 пересадки"/>
-            <Checkbox name="3 пересадки"/>
+            <Checkbox name="Все" id={1}/>
+            <Checkbox name="Без пересадок" id={2}/>
+            <Checkbox name="1 переcадка" id={3}/>
+            <Checkbox name="2 пересадки" id={4}/>
+            <Checkbox name="3 пересадки" id={5}/>
         </div>
     );
 }
