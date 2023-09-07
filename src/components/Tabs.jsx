3e3eb06ff3cd98eed0a521c9480889acee44acd3
Shamil Kh <shamilist90@gmail.com>
2023-09-07 21:57:01 +0300

    f

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -4,7 +4,7 @@ import { Tab } from './Tab'
 export const Tabs = () => {
     return (
         <ul className='tabs'>
-        <Tab name='right_tab' text='САМЫЙ ДЕШЕВЫЙ'/>
+        <Tab name='right_tab active_tab' text='САМЫЙ ДЕШЕВЫЙ'/>
         <Tab name='center_tab' text='САМЫЙ БЫСТРЫЙ'/>
         <Tab name='left_tab' text='ОПТИМАЛЬНЫЙ'/>
         </ul>
